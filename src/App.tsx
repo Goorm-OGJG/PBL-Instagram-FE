b9151bd60ca3bc83817cc5cd2a92e4230dec151c
Narcoker <jykim9868@gmail.com>
2023-08-03 17:53:02 +0900

    ✨ Feat: 페이지 라우팅 구현 완료
    Closed #10

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import React from "react";
-import New from "./pages/New/New";
 import { Route, Routes } from "react-router";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
+import FindPassword from "./pages/FindPassword/FindPassword";
+import Home from "./pages/Home/Home";
+import Story from "./pages/Story/Story";
+import SearchTag from "./pages/SearchTag/SearchTag";
+import Feed from "./pages/Feed/Feed";
+import Profile from "./pages/Profile/Profile";
+import EditProfile from "./pages/EditProfile/EditProfile";
+import SavedProfile from "./pages/SavedProfile/SavedProfile";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<New />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/signup" element={<SignUp />}></Route>
+      <Route path="/help/password" element={<FindPassword />}></Route>
+      <Route path="/Home" element={<Home />}></Route>
+      <Route path="/stories/:userName/:storyId" element={<Story />}></Route>
+      <Route path="/explore/tags/:query" element={<SearchTag />}></Route>
+      {/* 예비 */}
+      <Route path="/feed/:feedId" element={<Feed />}></Route>
+      <Route path="/accounts/:userName" element={<Profile />}></Route>
+      <Route path="/accounts/:userName/edit" element={<EditProfile />}></Route>
+      {/* 예비 */}
+      <Route path="/accounts/:userName/feed/saved" element={<SavedProfile />}></Route>
     </Routes>
   );
 }
---
+++ src/pages/SavedProfile/SavedProfile.tsx
@@ -0,0 +1,5 @@
+function SavedProfile() {
+  return <div>SavedProfile</div>;
+}
+
+export default SavedProfile;
