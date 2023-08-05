7e609e61de058763925ff1d26c86fb4205be4c50
Narcoker <jykim9868@gmail.com>
2023-08-05 20:57:34 +0900

    💄 Feat: 로그인 페이지 UI 구현 완료
    입력 폼에 값 입력 시 placeholder 애니메이션 적용
    모든 입력 폼에 값이 있어야 로그인 버튼 활성화
    
    Fixes: #7

---
+++ src/components/InputBox/InputBox.style.tsx
@@ -0,0 +1,49 @@
+import { styled } from "styled-components";
+import * as COLOR from "../../constants/color";
+import * as FONT from "../../constants/font";
+export const InputBoxWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  border-radius: 5px;
+`;
+
+export const PlaceHolder = styled.span<{ isFocused: boolean }>`
+  position: absolute;
+  top: 50%;
+  transform: ${({ isFocused }) => (isFocused ? "translateY(-170%)" : "translateY(-70%)")};
+  left: 5px;
+  font-size: ${({ isFocused }) => (isFocused ? FONT.XS : FONT.S)};
+  color: ${COLOR.Gray3};
+  pointer-events: none;
+  transition: 0.3s;
+`;
+
+export const InputBox = styled.input<{ isFocused: boolean }>`
+  border: 1px solid ${COLOR.BorderColor};
+  height: 38px;
+  margin-bottom: 10px;
+  outline: none;
+  width: inherit;
+  padding-left: 5px;
+  padding-top: ${({ isFocused }) => (isFocused ? "12px" : "0px")};
+  background-color: ${COLOR.BackgroundColor};
+
+  &:focus {
+    border: 1px solid ${COLOR.Gray2};
+  }
+`;
+
+export const VisiablePassword = styled.button`
+  position: absolute;
+  background-color: transparent;
+  border: none;
+  top: 50%;
+  right: 0;
+  transform: translateY(-70%);
+  font-size: ${FONT.S};
+  font-weight: ${FONT.Bold};
+  white-space: nowrap;
+  &:hover {
+    color: ${COLOR.Gray3};
+  }
+`;
