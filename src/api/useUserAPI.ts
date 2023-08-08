41480bb4cd728df4fad81912dfa801cdbea3efce
Narcoker <jykim9868@gmail.com>
2023-08-08 18:34:45 +0900

    ✨ Feat: Axios Request Type 및 Response Type 구조화

--- src/api/useUserAPI.ts
+++ src/api/useUserAPI.ts
@@ -1,15 +1,10 @@
+import * as T from "../types/request/user.request";
 import axios from "./axios";
 import { useNavigate } from "react-router";
 
-export interface LoginPayloadType {
-  userName: string;
-  password: string;
-  type: string;
-}
-
 export function useUserAPI() {
   const navigate = useNavigate();
-  const requestLogin = (payload: LoginPayloadType) => {
+  const requestLogin = (payload: T.LoginPayloadType) => {
     axios
       .post(`${import.meta.env.VITE_API_URL}/api/users/login`, payload)
       .then((response) => {
@@ -21,5 +16,40 @@ export function useUserAPI() {
       });
   };
 
-  return { requestLogin };
+  function requestSignUp(payload: T.SignUpPayloadType) {
+    axios
+      .post(`${import.meta.env.VITE_API_URL}/api/`, payload)
+      .then(() => {
+        alert("회원가입 되었습니다.");
+        navigate("/login");
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  }
+
+  function requestIsEqualCertNumber(payload: T.IsEqualCertNumberPayloadType) {
+    axios
+      .post(`${import.meta.env.VITE_API_URL}/api/`, payload)
+      .then(() => {
+        navigate("/help/newpassword");
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  }
+
+  function requestSetPassword(payload: T.SetPasswordType) {
+    axios
+      .post(`${import.meta.env.VITE_API_URL}/api/`, payload)
+      .then(() => {
+        alert("비밀번호가 변경되었습니다.");
+        navigate("/login");
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  }
+
+  return { requestLogin, requestSignUp, requestIsEqualCertNumber, requestSetPassword };
 }
