3e2d776efff945cfa3418e6ca01d1a7aeb4331ce
Narcoker <jykim9868@gmail.com>
2023-08-08 18:34:45 +0900

    ✨ Feat: Axios Request Type 및 Response Type 구조화

---
+++ src/types/request/user.request.ts
@@ -0,0 +1,30 @@
+export interface LoginPayloadType {
+  userName: string;
+  password: string;
+  type: string;
+}
+
+export interface SignUpPayloadType {
+  email: string;
+  userName: string;
+  nickname: string;
+  password: string;
+}
+
+export interface IsEqualCertNumberPayloadType {
+  userName: string;
+  type: string;
+  validate: string;
+}
+
+export interface SignUpPayloadType {
+  userName: string;
+  type: string;
+  password: string;
+}
+
+export interface SetPasswordType {
+  userName: string;
+  type: string;
+  password: string;
+}
