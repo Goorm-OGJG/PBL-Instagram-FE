60d839222f95dc7814603273be8c47e27677e470
james.joe <kyeun950830@gmail.com>
2023-08-12 03:15:36 +0900

    ✨ Feat: 프로필 헤더 영역 조건별 구현, API 설정
    Fixes: #40

--- src/pages/Profile/components/ProfileHeader.tsx
+++ src/pages/Profile/components/ProfileHeader.tsx
@@ -1,7 +1,13 @@
 import * as S from "./ProfileHeader.style";
-import { useNavigate, useParams } from "react-router-dom";
-import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router";
+import { useState, useEffect } from "react";
 import FollowerModal from "./FollowerModal";
+import { useRecoilValue } from "recoil";
+import {ProfileState } from "../../../recoil/profileState";
+import { ProfileResponseType } from "../../../types/client/profile.client";
+// import useFollowAPI from "../../../api/useFollowAPI";
+// import * as T from "../../../types/client/follower.client";
 
 interface Account {
   id: number;
@@ -11,7 +17,7 @@ interface Account {
   followerCount: number;
   followingCount: number;
   feedCount: number;
-  followingStatus: string;
+  followingStatus: boolean;
   secretStatus: string;
 }
 
@@ -25,16 +31,41 @@ const accounts: Account[] = [
     followerCount: 103,
     followingCount: 130,
     feedCount: 42,
-    followingStatus: "팔로잉 상태",
+    followingStatus: false,
     secretStatus: "비공개 상태",
   },
 ];
 
 function ProfileHeader() {
+  const localIdString = localStorage.getItem('userId');
+  const localId = localIdString !== null ? parseInt(localIdString) : null; // localStorage 값
   const { nickname } = useParams();
   const navigate = useNavigate();
-  const [followerModal, setFollowerModal] = useState(false);
-  const [followModal, setFollowModal] = useState(false);
+  const [followerModal, setFollowerModal] = useState<boolean>(false);
+  const [followModal, setFollowModal] = useState<boolean>(false);
+  const profileInfo = useRecoilValue<ProfileResponseType>(ProfileState); // 받아온 프로필 정보 데이터
+  const profileUserId = profileInfo.userId;
+  const isSecret = profileInfo.isSecret;
+  //🔥 const {requestDeleteFollower} = useFollowAPI();
+  //🔥 const [blueColor, setBlueColor] = useState<boolean>(false);
+  //🔥 const handleCompareNickName = () => {
+  //   if (localId === profileUserId) {
+  //     navigate(`/accounts/${nickname}/edit`);
+  //   } else if (
+    //     profileInfo.followingStatus === false
+    //     localId !== profileUserId &&
+  //   ) {
+  //     setBlueColor(true);
+  //     requestPostFollowing(localId,profileUserId);
+  //   } else if (
+  //     localId !== profileUserId &&
+  //     profileInfo.followingStatus === true
+  //   ) {
+  //     requestDeleteFollower(localId,profileUserId);
+  //     console.log("팔로우 취소 delete");
+  //   }
+  // };
+  
   return (
     <S.ProfileWrapper>
       <S.ProfileHeader>
@@ -42,6 +73,7 @@ function ProfileHeader() {
           <S.UserImgInnerBox>
             <S.UserButton>
               <S.UserImg src={accounts[0].profileImg} alt="profileImg" />
+              {/*🔥 profileInfo.profileImg */}
             </S.UserButton>
           </S.UserImgInnerBox>
         </S.UserImgBox>
@@ -50,25 +82,32 @@ function ProfileHeader() {
             <S.UserNickName>
               <S.NickName>{nickname}</S.NickName>
             </S.UserNickName>
-            <S.EditProfileBtn
+            <S.EditProfileBtn 
               onClick={() => {
                 navigate(`/accounts/${nickname}/edit`);
+                // 🔥 handleCompareNickName();
               }}
             >
               프로필 편집
+              {/* 🔥 {localId === profileUserId?"프로필 편집"
+                : profileInfo.followingStatus === false
+                ? "팔로우 하기" : "팔로우 취소"} */}
             </S.EditProfileBtn>
           </S.InfoHeader>
           <S.InfoFollowBox>
             <S.UserPost>게시물 {accounts[0].feedCount}</S.UserPost>
+            {/*🔥 profileInfo.feedCount */}
             <S.UserFollowing
               onClick={() => {
                 setFollowerModal((prev) => !prev);
               }}
             >
               팔로워 {accounts[0].followerCount}
+              {/* 🔥 profileInfo.followerCount */}
             </S.UserFollowing>
             {followerModal && (
               <FollowerModal
+                //🔥 followerData={followerData}
                 followerModal={followerModal}
                 setFollowerModal={setFollowerModal}
                 followModal={followModal}
@@ -82,6 +121,7 @@ function ProfileHeader() {
             >
               {" "}
               팔로우 {accounts[0].followingCount}
+              {/*🔥 profileInfo.followingCount */}
             </S.UserFollower>
             {followModal && (
               <FollowerModal
@@ -93,6 +133,7 @@ function ProfileHeader() {
             )}
           </S.InfoFollowBox>
           <S.UserIntro>{accounts[0].userIntro}</S.UserIntro>
+          {/* 🔥{profileInfo.userIntro} */}
         </S.ProfileInfoBox>
       </S.ProfileHeader>
     </S.ProfileWrapper>
